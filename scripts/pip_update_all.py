1db3e69efbcf968deaab251b86c79ba6b4f41977
Kiwis in Space <hq@kiwisinspace.co.nz>
2015-06-12 22:43:04 +1200

    Added cache path

--- scripts/pip_update_all.py
+++ scripts/pip_update_all.py
@@ -6,9 +6,11 @@ from datetime import datetime
 
 ts = datetime.now().strftime('%Y-%m-%d_%H%M%S')
 fn = "pip_freeze_%s.txt" % ts
+dn = "C:\\vol\\cache\\pypi"
 
 with open(fn, 'a') as the_file:
     for dist in pip.get_installed_distributions():
         the_file.write("%s==%s\n" % (dist.project_name, dist.version))
+        # call("pip install --download %s %s" % (dn, dist.project_name), shell=True)
         call("pip install --upgrade " + dist.project_name, shell=True)
         print "==> Upgraded", dist.project_name
